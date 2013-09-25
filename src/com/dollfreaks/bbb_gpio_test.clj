14bf22410d699b404780aa88b0862cb384baa64a
Darrell Hamilton <darrell.noice@gmail.com>
2013-09-24 23:57:19 -0700

    initial test body

--- src/com/dollfreaks/bbb_gpio_test.clj
+++ src/com/dollfreaks/bbb_gpio_test.clj
@@ -1,7 +1,15 @@
 (ns com.dollfreaks.bbb-gpio-test
-  (:gen-class))
+  (:gen-class)
+  (:require [com.dollfreaks.gpio.pin :as pin]))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [p 66]
+    (pin/export! p)
+    (prn "exported?" (pin/exported? p))
+    (pin/output! p)
+    (prn "output?" (pin/output? p))
+    (prn ":high?" (pin/high? p))
+    (pin/unexport! p)
+    (prn "exported?" (pin/exported? p))
+    (println "Hello, World!")))
